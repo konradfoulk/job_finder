208bc65fe04fe5eca6bdbcbd3c07ff1b50ed0413
konradfoulk <konrad.foulk@gmail.com>
2025-08-09 12:05:12 -0400

    update .gitignore

--- main.py
+++ main.py
@@ -2,6 +2,5 @@ import adzuna
 import fantastic
 
 # example scripts
-adzuna.find_jobs('Jr. Web Developer', pages=10, file_name='exisitng_csv')
 fantastic.find_jobs('developer | \'software engineer\' | ((\'full stack\' | frontend | backend) & engineer)',
-                    'Remote OK,Remote Solely', '0-2', file_name='fantastic_jobs')
+                    'Remote OK,Remote Solely', '0-2', 100)
